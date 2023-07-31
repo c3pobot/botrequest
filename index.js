cc1194545b85f268c53b27b7b4c56962f04f7c19
scuba <scuba75@gmail.com>
2023-07-31 00:26:15 +0000

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
+const log = require('./logger')
+let logLevel = process.env.LOG_LEVEL || log.Level.INFO;
+log.setLevel(logLevel);
 const fetch = require('./fetch')
-const getNumShards = require('./getNumShards')
 const BOT_NODE_NAME_PREFIX = process.env.BOT_NODE_NAME_PREFIX || 'bot'
 const BOT_SVC = process.env.BOT_SVC || 'bot:3000'
 
@@ -23,7 +25,7 @@ const requestWithRetry = async(uri, opts = {}, count = 0)=>{
         count++
         return await requestWithRetry(uri, opts, count)
       }else{
-        throw(`tried request ${count} time and errored with ${res.error} : ${res.message}`)
+        throw(`tried request ${count} time(s) and errored with ${res.error} : ${res.message}`)
       }
     }
     return res
@@ -36,9 +38,10 @@ const monitorNumShards = async()=>{
     let opts = { timeout: 5000, compress: true, method: 'GET' }
     let res = await requestWithRetry(`http://${BOT_SVC}/getNumShards`, opts)
     if(res?.body?.totalShards && +res.body.totalShards > 0){
-      if(notify) console.log('Setting number of shards to '+res.body.totalShards+'...')
-      BOT_TOTAL_SHARDS = +res.body.totalShards
-      notify = false
+      if(BOT_TOTAL_SHARDS !== +res.body.totalShards){
+        log.info(`Change in number of ${BOT_NODE_NAME_PREFIX} shards from ${BOT_TOTAL_SHARDS} to ${res.body.totalShards}`)
+        BOT_TOTAL_SHARDS = res.body.totalShards
+      }
     }
     setTimeout(monitorNumShards, 5000)
   }catch(e){
@@ -53,7 +56,7 @@ module.exports = async(cmd, opts = {})=>{
     let podName = opts.podName
     if(!podName) podName = await getPodName(opts)
     if(!podName) throw('Error getting podName...')
-    let payload = {method: 'POST', timeout: 60000, compress: true, headers: {"Content-Type": "application/json"}}
+    let payload = { method: 'POST', timeout: 60000, compress: true, headers: {"Content-Type": "application/json"} }
     payload.body = JSON.stringify({ ...opts, ...{ cmd: cmd, podName: podName } })
     let res = await requestWithRetry(`http://${podName}.${BOT_SVC}/cmd`, payload)
     if(res?.body) return res.body
